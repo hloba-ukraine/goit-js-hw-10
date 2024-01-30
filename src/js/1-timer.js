f288eb2fffdb309f7501399698baf966d8a0099e
hloba-ukraine <138844087+hloba-ukraine@users.noreply.github.com>
2024-01-29 23:41:42 -0700

    1 task

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,14 +1,43 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import iconClose from '../img/bi_x-octagon.png';
+
+const refs = {
+  inputEl: document.querySelector('.input-field'),
+  btnStart: document.querySelector('.start-btn'),
+  dataDay: document.querySelector('span[data-days]'),
+  dataHours: document.querySelector('span[data-hours]'),
+  dataMinutes: document.querySelector('span[data-minutes]'),
+  dataSeconds: document.querySelector('span[data-seconds]'),
+};
+let userSelectedDate;
+let difference;
+let timerInterval;
+refs.btnStart.disabled = true;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    userSelectedDate = selectedDates[0];
+    difference = userSelectedDate.getTime() - Date.now();
+    if (userSelectedDate < Date.now()) {
+      iziToast.show({
+        message: 'Please choose a date in the future',
+        messageColor: '#FFF',
+        backgroundColor: '#FF0000',
+        position: 'topRight',
+        iconUrl: iconClose,
+      });
+    } else {
+      refs.btnStart.disabled = false;
+    }
   },
 };
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -27,3 +56,25 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+function padNumber(value) {
+  return value.toString().padStart(2, '0');
+}
+function onTimer(difference) {
+  const timer = convertMs(difference);
+  refs.dataDay.textContent = `${padNumber(timer.days)}`;
+  refs.dataHours.textContent = `${padNumber(timer.hours)}`;
+  refs.dataMinutes.textContent = `${padNumber(timer.minutes)}`;
+  refs.dataSeconds.textContent = `${padNumber(timer.seconds)}`;
+}
+function onStart() {
+  timerInterval = setInterval(() => {
+    if (difference <= 0) {
+      clearInterval(timerInterval);
+    } else {
+      onTimer(difference);
+      difference -= 1000;
+    }
+  }, 1000);
+}
+flatpickr('#datetime-picker', options);
+refs.btnStart.addEventListener('click', onStart);
